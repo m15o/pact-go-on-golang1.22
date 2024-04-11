a12d90082cb866d73b139e9554566354a37dffdb
Masanori Matsumoto <masanori_matsumoto@cybozu.co.jp>
2024-04-11 16:56:11 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module pact-go-test
+
+go 1.22
+
+require (
+	github.com/pact-foundation/pact-go/v2 v2.0.4
+	github.com/stretchr/testify v1.8.4
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
+	google.golang.org/grpc v1.60.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ sample_test.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/v2/consumer"
+	"github.com/pact-foundation/pact-go/v2/log"
+	"github.com/pact-foundation/pact-go/v2/matchers"
+	"github.com/pact-foundation/pact-go/v2/models"
+	"github.com/stretchr/testify/assert"
+)
+
+type S = matchers.S
+
+var Like = matchers.Like
+var EachLike = matchers.EachLike
+var Term = matchers.Term
+var Regex = matchers.Regex
+var HexValue = matchers.HexValue
+var Identifier = matchers.Identifier
+var IPAddress = matchers.IPAddress
+var IPv6Address = matchers.IPv6Address
+var Timestamp = matchers.Timestamp
+var Date = matchers.Date
+var Time = matchers.Time
+var UUID = matchers.UUID
+var ArrayMinLike = matchers.ArrayMinLike
+
+type Map = matchers.MapMatcher
+
+var Decimal = matchers.Decimal
+var Integer = matchers.Integer
+var Equality = matchers.Equality
+var Includes = matchers.Includes
+var FromProviderState = matchers.FromProviderState
+var EachKeyLike = matchers.EachKeyLike
+var ArrayContaining = matchers.ArrayContaining
+var ArrayMinMaxLike = matchers.ArrayMinMaxLike
+var ArrayMaxLike = matchers.ArrayMaxLike
+var DateGenerated = matchers.DateGenerated
+var TimeGenerated = matchers.TimeGenerated
+var DateTimeGenerated = matchers.DateTimeGenerated
+
+func TestConsumerV4(t *testing.T) {
+	log.SetLogLevel("TRACE")
+
+	mockProvider, err := consumer.NewV4Pact(consumer.MockHTTPProviderConfig{
+		Consumer: "PactGoV4Consumer",
+		Provider: "V4Provider",
+		Host:     "127.0.0.1",
+		TLS:      true,
+	})
+	assert.NoError(t, err)
+
+	// Set up our expected interactions.
+	err = mockProvider.
+		AddInteraction().
+		Given("state 1").
+		GivenWithParameter(models.ProviderState{
+			Name: "User foo exists",
+			Parameters: map[string]interface{}{
+				"id": "foo",
+			},
+		}).
+		UponReceiving("A request to do a foo").
+		WithRequest("POST", "/foobar", func(b *consumer.V4RequestBuilder) {
+			b.
+				Header("Content-Type", S("application/json")).
+				Header("Authorization", Like("Bearer 1234")).
+				Query("baz", Regex("bar", "[a-z]+"), Regex("bat", "[a-z]+"), Regex("baz", "[a-z]+")).
+				JSONBody(Map{
+					"id":       Like(27),
+					"name":     FromProviderState("${name}", "billy"),
+					"lastName": Like("billy"),
+					"datetime": DateTimeGenerated("2020-01-01T08:00:45", "yyyy-MM-dd'T'HH:mm:ss"),
+				})
+
+		}).
+		WillRespondWith(200, func(b *consumer.V4ResponseBuilder) {
+			b.
+				Header("Content-Type", S("application/json")).
+				JSONBody(Map{
+					"datetime":       Regex("2020-01-01", "[0-9\\-]+"),
+					"name":           S("Billy"),
+					"lastName":       S("Sampson"),
+					"superstring":    Includes("foo"),
+					"id":             Integer(12),
+					"accountBalance": Decimal(123.76),
+					"itemsMinMax":    ArrayMinMaxLike(27, 3, 5),
+					"itemsMin":       ArrayMinLike("thereshouldbe3ofthese", 3),
+					"equality":       Equality("a thing"),
+					"arrayContaining": ArrayContaining([]interface{}{
+						Like("string"),
+						Integer(1),
+						Map{
+							"foo": Like("bar"),
+						},
+					}),
+				})
+		}).
+		ExecuteTest(t, test)
+	assert.NoError(t, err)
+}
+
+var test = func() func(config consumer.MockServerConfig) error {
+	return rawTest("baz=bat&baz=foo&baz=something")
+}()
+
+var rawTest = func(query string) func(config consumer.MockServerConfig) error {
+
+	return func(config consumer.MockServerConfig) error {
+
+		config.TLSConfig.InsecureSkipVerify = true
+		client := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: config.TLSConfig,
+			},
+		}
+		req := &http.Request{
+			Method: "POST",
+			URL: &url.URL{
+				Host:     fmt.Sprintf("%s:%d", "localhost", config.Port),
+				Scheme:   "https",
+				Path:     "/foobar",
+				RawQuery: query,
+			},
+			Body:   io.NopCloser(strings.NewReader(`{"id": 27, "name":"billy", "lastName":"sampson", "datetime":"2021-01-01T08:00:45"}`)),
+			Header: make(http.Header),
+		}
+
+		// NOTE: by default, request bodies are expected to be sent with a Content-Type
+		// of application/json. If you don't explicitly set the content-type, you
+		// will get a mismatch during Verification.
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer 1234")
+
+		_, err := client.Do(req)
+
+		return err
+	}
+}
